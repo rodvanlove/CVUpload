3601d23535c910122db64f01b695a0618f12cf9f
rodlo <rodlo@ROD-PC>
2022-12-05 15:26:12 +0000

    Stopped random filename

---
+++ CVUpload/Pages/Thanks.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CVUpload.Pages
+{
+    public class ThanksModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
